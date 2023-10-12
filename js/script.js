016accbb09c201b8b1030724bc570e66473afa55
Finnedorn <daniele.giuntoli@outlook.com>
2023-10-12 16:32:08 +0200

    js: fixed mail checker message not properly showing in case of success

--- js/script.js
+++ js/script.js
@@ -45,30 +45,52 @@ const mail = [
     'reporepo.ciao@hotmail.it'
 ]
 
+
+
 //creo una funzione che al click del bottone mi faccia un check delle mail dell'array
 search.addEventListener('click', function() {
     //creo un ciclo che passi in rassegna a tutti gli elementi della lista
+
+    //creo una let che mi permetta di attivare il messaggio
+    //se non avessi questa let il sistema poi quando andrebbe a fare l'if, per dare un messaggio positivo dovrebbe avere una corrrispondenza completa tra il value e tutti gli array che sarebbe impossibile
+    //linkando tutto all'attivazione true false di questa singola let infatti basta che una sola delle condizioni si verifichi per attivarla
+    let checked = false
+
+    //creo una let linkata al div dove inserirò il messaggio
+    let result = document.getElementById('result');
+
     for(let i = 0; i < mail.length; i++) {
         //creo una let per associare i a tutti i valori dell'array
         let mailcorrect = mail[i];
-        //creo una let linkata al div dove inserirò il messaggio
-        let result = document.getElementById('result');
 
         console.log(mailcorrect);
         
-        //creo un if: se mail correct corrisponde a mailcheck cioè il value inserito allora darò un messaggio positivo, altrimenti uno negativo
+        //creo un if: in questo if metto che il value deve essere uguale ad ogni singolo elemento dell'array, impossibile, ma in quesot caso mi basta una sola singola corrispondenza per attivare il messaggio positivo, altrimenti uno negativo
         if (mailcorrect === mailcheck) {
 
-            result.innerHTML = 'perfetto!';
+            let checked = true;
 
         } else {
-            result.innerHTML = 'email non corrispondente!';
+
+            let checked = false;
         }
-    
     }
+    //ora setto il messagio in base al true false della let checked 
+    if (checked = true) { 
+        
+        result.innerHTML = 'Perfetto!'
+    } else {
+        result.innerHTML = 'Email non corrispondente!'
+    }
+})
+
+cancel.addEventListener('click', function() {
+
 })
 
 //dice
 
-//creo una 
-
+//creo una const che generi un numero da 1 a 6 per il giocatore 
+const playerscore = document.getElementById('playerscore');
+//creo una const che geenri un numero da 1 a 6 per la cpu
+const cpuscore = document.getElementById('cpuscore');
