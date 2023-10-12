29b4c65f65c4fbdb077209b6be8ac90fad4ea50e
Finnedorn <daniele.giuntoli@outlook.com>
2023-10-12 15:55:57 +0200

    html: added mail and dice page/ js: added mail search feature

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 Gioco dei dadi
 
-
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda:
@@ -25,5 +24,51 @@ Consigli del giorno:
 
 Bonus
 Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
-  2.   Grafica per il gioco dei dadi
-  */
+2.   Grafica per il gioco dei dadi
+*/
+
+
+//creo una const e la linko al value del form
+const mailcheck = document.getElementById('mail').value;
+
+//creo una const che si allacci con l id del bottone per la funzione
+const search = document.getElementById('search');
+
+//creo una const che si allacci all'id del bottone annulla
+const cancel = document.getElementById('cancel');
+
+//creo un array con una lista di mail possibili
+const mail = [
+    'mariomario@gmail.com',
+    'nonsochescrivere@outlook.com',
+    'checimetto@gmail.com',
+    'reporepo.ciao@hotmail.it'
+]
+
+//creo una funzione che al click del bottone mi faccia un check delle mail dell'array
+search.addEventListener('click', function() {
+    //creo un ciclo che passi in rassegna a tutti gli elementi della lista
+    for(let i = 0; i < mail.length; i++) {
+        //creo una let per associare i a tutti i valori dell'array
+        let mailcorrect = mail[i];
+        //creo una let linkata al div dove inserirò il messaggio
+        let result = document.getElementById('result');
+
+        console.log(mailcorrect);
+        
+        //creo un if: se mail correct corrisponde a mailcheck cioè il value inserito allora darò un messaggio positivo, altrimenti uno negativo
+        if (mailcorrect === mailcheck) {
+
+            result.innerHTML = 'perfetto!';
+
+        } else {
+            result.innerHTML = 'email non corrispondente!';
+        }
+    
+    }
+})
+
+//dice
+
+//creo una 
+
